64c90fa69681c1f99aee60679d3e12c254015f86
Spencer Smith <smis45@U-1SCRGQKDDX84S.nwie.net>
2024-12-17 10:41:15 -0500

    Adding a template and starting day 6

---
+++ template/index.py
@@ -0,0 +1,22 @@
+import os
+
+def load(file_name):
+    print("Loading file...")
+
+
+def part_1():
+    print("Day X, Part 1")
+
+
+def part_2():
+    print("Day X, Part 2")
+
+def main(file_name):
+    print("Day X")
+    load(file_name)
+    part_1()
+    part_2()
+
+if __name__ == "__main__":
+    main(os.path.join(os.path.dirname(__file__), "input.txt"))
+    
